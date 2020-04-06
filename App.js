fc4296c74587e541e44893fc1929866bd53b7fcf
Sanjay Noronha <sanjay.noronha@walmartlabs.com>
2020-04-06 08:53:25 -0700

    Moved all code to src dir (except for index.js and App.js)

--- App.js
+++ App.js
@@ -6,28 +6,28 @@ import {createDrawerNavigator} from '@react-navigation/drawer';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 
-import HomeScreen from './screens/HomeScreen';
-import FavoritesScreen from './screens/FavoritesScreen';
-import StoreFinderScreen from './screens/StoreFinderScreen';
-import MoreScreen from './screens/MoreScreen';
-import WebViewScreen from './screens/WebViewScreen';
-import WebViewABScreen from './screens/WebViewABScreen';
-import {SearchScreen} from './screens/SearchScreen';
-import {SearchBaseScreen} from './screens/SearchScreen';
+import HomeScreen from './src/screens/HomeScreen';
+import FavoritesScreen from './src/screens/FavoritesScreen';
+import StoreFinderScreen from './src/screens/StoreFinderScreen';
+import MoreScreen from './src/screens/MoreScreen';
+import WebViewScreen from './src/screens/WebViewScreen';
+import WebViewABScreen from './src/screens/WebViewABScreen';
+import {SearchScreen} from './src/screens/SearchScreen';
+import {SearchBaseScreen} from './src/screens/SearchScreen';
 
-import ItemPageScreen from './screens/ItemPageScreen';
-import CartScreen from './screens/CartScreen';
+import ItemPageScreen from './src/screens/ItemPageScreen';
+import CartScreen from './src/screens/CartScreen';
 
-import {HeaderBackLink} from './components/header/HeaderBackLink';
-import {HeaderCartLink} from './components/header/HeaderCartLink';
-import {HeaderSearchBar} from './components/header/HeaderSearchBar';
-import {HamburgerPlusScreen} from './components/header/HamburgerMenu';
+import {HeaderBackLink} from './src/components/header/HeaderBackLink';
+import {HeaderCartLink} from './src/components/header/HeaderCartLink';
+import {HeaderSearchBar} from './src/components/header/HeaderSearchBar';
+import {HamburgerPlusScreen} from './src/components/header/HamburgerMenu';
 
-import {IconWithBadge} from './components/footer/IconWithBadge';
+import {IconWithBadge} from './src/components/footer/IconWithBadge';
 
 //-------- REDUX ---------//
 import {Provider} from 'react-redux';
-import store from './stores/store';
+import store from './src/stores/store';
 
 // Hack to ensure FontAwesome loads
 Icon.loadFont();
